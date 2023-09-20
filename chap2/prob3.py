c8129977e9628e02b187a4b963175549bc9c3d57
jimin <meerkat@cu.ac.kr>
2023-09-21 02:13:20 +0900

    chap2

---
+++ chap2/prob3.py
@@ -0,0 +1,9 @@
+name = input("Hi. What's your name? ")
+age = int(input("And how old are you? "))
+pound = int(input("Okay, last question. How many pounds do you weigh? "))
+
+print("\nDid you know that you're just",int(age/7) ,"in dog years? \n")
+print("But you're also over",age*365*24*60*60 ,"seconds old.\n")
+print("If a small child were trying to get your attention, your name would become: ",name*5, " \n")
+print("Did you know that on the moon you would weigh only",pound/6 ,"pounds? ")
+print("But on the sun, you'd weigh",pound*27.1 , "<but, ah... not for long>.")
