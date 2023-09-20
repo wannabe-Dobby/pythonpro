2209a2ed95bb3ae1d8c46031ca8fa417c392dc20
jimin <meerkat@cu.ac.kr>
2023-09-21 01:16:04 +0900

    chap2

--- chap2/prob2.py
+++ chap2/prob2.py
@@ -1,3 +1,18 @@
-print("Trust Fund Buddy\n\n\
-        Totals tour monthly spending so that your trust fund doesn't run out\n\
-        <and you're forced to get a real job>.")
+print("\t\t\tTrust Fund Buddy\n\n\
+Totals tour monthly spending so that your trust fund doesn't run out\n\
+<and you're forced to get a real job>.\n\n\
+Please enter the requested, monthly costs. since you're rich, ignore pennies\n\
+and use only dollars amounts.\n\n")
+
+car = int(input("Lamborghimi Tune-Ups: "))
+rent = int(input("Mantattan Apartment: "))
+jet = int(input("Private Jet Rental: "))
+gifts = int(input("Gifts: "))
+food = int(input("Dinning out: "))
+staff = int(input("Staff (butlers, chef, driver, assistant): "))
+guru = int(input("Personal Guru and coach: "))
+games = int(input("Computer Games: "))
+
+total = car + rent + jet + gifts + food + staff + guru + games
+
+print("\nGrand Total :",total)
