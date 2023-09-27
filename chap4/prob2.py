cdf85e37ad027537e1ba5648a9ee55efea4e8ef3
jimin <meerkat@cu.ac.kr>
2023-09-27 02:35:51 +0000

    chap4

--- chap4/prob2.py
+++ chap4/prob2.py
@@ -1,8 +1,21 @@
 import random
 
-hero HP = random.randrange(50, 100)
-monster HP = random.randrange(50, 100)
-hero attack value = random.randrange(-10, 20)
-monster attack value = random.randrange(-10, 20)
+hero_HP = random.randrange(50, 100)
+monster_HP = random.randrange(50, 100)
+hero_attack_value = random.randrange(-10, 20)
+monster_attack_value = random.randrange(-10, 20)
 
-print("hero Hp: ", hero HP,", moster HP: ", monster HP)
+print("hero Hp: ", hero_HP,", moster HP: ", monster_HP)
+
+if hero_attack_value > 0:
+  result1 = "success"
+else:
+  result1 = "fail"
+
+if monster_attack_value > 0:
+  result2 = "success"
+else:
+  result2 = "fail"
+
+while hero_HP > 0 or monster_HP > 0:
+  print("hero(HP:", hero_HP, ", attack:", hero_attack_value, ")", result1, " <-> monster(HP:", monster_HP, ", attack:", monster_attack_value, ")", result2)
--- chap4/prob3.py
+++ chap4/prob3.py
@@ -1,17 +1,17 @@
-print("\t\tWelcome to 'Guess My Number'!\n\
-        I'm thinking of a number between 1 and 100.\n\
-        Try to guess it in as few attempts as possible.\n")
+print("\tWelcome to 'Guess My Number'!\n\n\
+I'm thinking of a number between 1 and 100.\n\
+Try to guess it in as few attempts as possible.\n")
 
 import random
 
 Num = random.randrange(1, 100)
-vlaue = random.randrange(1, 100)
+value = random.randrange(1, 100)
 
-while value == "Num"
+while value == "Num":
 
-vlaue = random.randrange(1, 100)
+  print("Take a guess : ", value)
 
-print("Take a guess : ", value)
+  vlaue = random.randrange(1, 100)
 
   if value > Num:
     print("Lower...")
@@ -20,4 +20,4 @@ print("Take a guess : ", value)
   else:
     print("You guessed it! The number was 15\nAnd it only took you", cnt, "tries!")
 
-  cnt++;
+  cnt = cnt + 1
