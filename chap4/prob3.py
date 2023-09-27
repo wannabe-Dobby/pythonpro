000938371583e7b07911813dd2083391c66be808
jimin <meerkat@cu.ac.kr>
2023-09-27 03:11:47 +0000

    chap4

--- chap4/prob3.py
+++ chap4/prob3.py
@@ -5,19 +5,19 @@ Try to guess it in as few attempts as possible.\n")
 import random
 
 Num = random.randrange(1, 100)
-value = random.randrange(1, 100)
 
-while value == "Num":
+cnt = 0
+x = 0
 
-  print("Take a guess : ", value)
+while x != Num:
 
-  vlaue = random.randrange(1, 100)
+  cnt += 1
+  x = int(input("Take a guess: "))
 
-  if value > Num:
+  if x > Num:
     print("Lower...")
-  elif value < Num:
+  elif x < Num:
     print("Upper...")
   else:
-    print("You guessed it! The number was 15\nAnd it only took you", cnt, "tries!")
-
-  cnt = cnt + 1
+    print("You guessed it! The number was", Num)
+    print("And it only took you", cnt, "tries!")
