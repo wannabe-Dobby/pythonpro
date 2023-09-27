d22c16128e3c363f2bed37e9fc64eca243f8cbe6
jimin <meerkat@cu.ac.kr>
2023-09-27 15:27:51 +0900

    chap4

--- chap4/prob2.py
+++ chap4/prob2.py
@@ -17,5 +17,17 @@ if monster_attack_value > 0:
 else:
   result2 = "fail"
 
+cnt = 0
+
 while hero_HP > 0 or monster_HP > 0:
   print("hero(HP:", hero_HP, ", attack:", hero_attack_value, ")", result1, " <-> monster(HP:", monster_HP, ", attack:", monster_attack_value, ")", result2)
+
+  cnt = cnt + 1
+
+  if hero_attack_damage > 0 or monster_attack_damage > 0:
+  hero_HP = Hero_HP - monster_attack_damage
+  monster_HP = monster_HP - hero_attack_damage
+
+print("Total phase: ", cnt)
+
+if hero_HP <= 0
