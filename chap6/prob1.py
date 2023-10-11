25e434e703cfe6d5097a616d36b79a09f4476a78
jimin <meerkat@cu.ac.kr>
2023-10-12 01:45:54 +0900

    chap6

--- chap6/prob1.py
+++ chap6/prob1.py
@@ -4,20 +4,24 @@ print("Your items:")
 for item in inventory:
   print(item)
 
-print("\nPress the enter key to continue.\nYou have", len(inventory), "items in yout possession.")
+input("\nPress the enter key to continue.")
+print("You have", len(inventory), "items in yout possession.")
 
+input("\nPress the enter key to continue.")
 if "healing potion" in inventory:
-  print("\nPress the enter key to continue.\nYou will live to fight another day.\n")
+  print("You will live to fight another day.\n")
 
 num = int (input("Enter the index number for an item in inventory: "))
 print("At index", num, "is", inventory[num], "\n")
 
-a = int (input("Enter the index number for an item in inventory: "))
-b = int (input("Enter the index number for an item in inventory: "))
+a = int (input("Enter the index number to begin a slice: "))
+b = int (input("Enter the index number to end the slice: "))
 
 print("inventory[", a, ":", b, "]\t", inventory[a:b])
 
-print("\nPress the enter key to continue.\nYou find a chest. It contains:")
+
+input("\nPress the enter key to continue.")
+print("You find a chest. It contains:")
 chest = ["gold", "gems"]
 print(chest)
 
@@ -25,18 +29,22 @@ inventory += chest
 print("You add the contents contents of the chest to your inventory.\nYour inventory is now:")
 print(inventory)
 
-print("\nPress the enter key to continue.\nYou trade your sword for crossbow.\nYour inventory is now:")
+input("\nPress the enter key to continue.")
+print("You trade your sword for crossbow.\nYour inventory is now:")
 inventory[0] = "crossbow"
 print(inventory)
 
-print("\nPress the enter key to continue.\nYou use your gold and gems to buy an orb of future telling.\nYour inventory is now:")
+input("\nPress the enter key to continue.")
+print("You use your gold and gems to buy an orb of future telling.\nYour inventory is now:")
 inventory[4:6] = ["orb of future telling"]
 print(inventory)
 
-print("\nPress the enter key to continue.\nIn a great battle, your shield is desttoyed.\nYour inventory is now:")
+input("\nPress the enter key to continue.")
+print("In a great battle, your shield is destroyed.\nYour inventory is now:")
 del inventory[2]
 print(inventory)
 
-print("\nPress the enter key to continue.\nYour crossbow and armor are stolen by thieves.\nYour inventory is now:")
+input("\nPress the enter key to continue.")
+print("Your crossbow and armor are stolen by thieves.\nYour inventory is now:")
 del inventory[:2]
 print(inventory)
