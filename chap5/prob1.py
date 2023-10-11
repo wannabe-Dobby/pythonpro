080a5090ba5c2ca293504e04bb1cb9b8601edf29
jimin <meerkat@cu.ac.kr>
2023-10-12 01:54:03 +0900

    chap5

--- chap5/prob1.py
+++ chap5/prob1.py
@@ -4,10 +4,12 @@ print("Your items:")
 for item in inventory:
   print(item)
 
-print("\nPress the enter key to continue.\nYou have", len(inventory), "items in your possession.")
+input("\nPress the enter key to continue.")
+print("You have", len(inventory), "items in your possession.")
 
-"healing portion" in inventory
-print("\nPress the enter key to continue.\nYou will live to fight another day\n")
+input("\nPress the enter key to continue.")
+if "healing potion" in inventory:
+    print("You will live to fight another day\n")
 
 num = int(input("Enter the index number for an item in inventory: "))
 print("At index", num, "is", inventory[num], "\n")
@@ -16,7 +18,9 @@ a = int(input("Enter the index number to begin a slice: "))
 b = int(input("Enter the index number to end the slice: "))
 print("inventory[", a, ":", b, "]\t", inventory[a:b])
 
-print("\nPress the enter key to continue.\nYou find a chest. It contains:")
+
+input("\nPress the enter key to continue.")
+print("You find a chest. It contains:")
 chest = ("gold", "gems")
 print(chest)
 
