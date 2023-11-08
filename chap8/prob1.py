f634fdf9288a7d584ff679628ebf067c15d6d4d2
wannabe-Dobby <131097975+wannabe-Dobby@users.noreply.github.com>
2023-11-08 01:51:12 +0000

    chap8

--- chap8/prob1.py
+++ chap8/prob1.py
@@ -1,8 +1,39 @@
 print("Opening and closing the file.\n")
+text_file = open("read_it.txt", "r")
+text_file.close()
 
 text_file = open("read_it.txt", "r")
-
+print("Reading characters from the file.")
 print(text_file.read(1))
+print(text_file.read(5))
+text_file.close()
 
+text_file = open("read_it.txt", "r")
+print("\nReading the entire file at once.")
+whole_thing = text_file.read()
+print(whole_thing)
 
+text_file = open("read_it.txt", "r")
+print("\nRead the characters from a line.")
+print(text_file.readline(1))
+print(text_file.readline(5))
+text_file.close()
+
+text_file = open("read_it.txt", "r")
+print("\nReading one line at a time.")
+print(text_file.readline())
+print(text_file.readline())
+print(text_file.readline())
+text_file.close()
+
+text_file = open("read_it.txt", "r")
+print("\nReading the entire file into a list.")
+lines = text_file.readlines()
+print(lines)
+text_file.close()
+
+text_file = open("read_it.txt", "r")
+print("\nLooping though the file, line by line.")
+for line in text_file:
+    print(line)
 text_file.close()
---
+++ chap8/prob2.py
@@ -0,0 +1,15 @@
+print("Createing a text file with the write() method.")
+text_file = open("write_it.txt", "w")
+text_file.write("Line 1\n")
+text_file.write("This is line 2\n")
+text_file.write("That makes this line 3\n")
+
+print("Creating a text file with the writelines() method.")
+text_file = open("write_it.txt", "w")
+lines = ["Line 1\n", "This is line 2\n", "That makes this line 3\n"]
+text_file.writelines(lines)
+
+print("Read the newly created file.")
+text_file = open("write_it.txt", "r")
+for line in text_file:
+    print(line)
